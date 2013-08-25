c5fcb508ec405c8f608a440dafc23930272cce8a
With Jive <withjive@icloud.com>
2013-08-25 18:22:10 +0700

    Auto deployment

--- packages/install.p7zip.sh
+++ packages/install.p7zip.sh
@@ -2,9 +2,13 @@
 
 
 function install_p7zip(){
-    cd $ROCK_DIR/3rdparty/p7zip
 
-    make -j4
-    make install
+    cd $ROCK_DIR/3rdparty/p7zip
+    echo -e "Compiling p7zip" | indent
+    make -j4 -s
+    echo -e "Installing p7zip" | indent
+    make install -s
+
+    ls /app/vendor/p7zip
 
 }
