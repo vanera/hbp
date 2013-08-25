ccd93d78bc69e25ebbd3d2d7a76b4c2cc6a55579
With Jive <withjive@icloud.com>
2013-08-25 17:44:59 +0700

    Auto deployment

--- packages/bundle.package.sh
+++ packages/bundle.package.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-function bundle_package(){
+function bundle_install(){
     
     PHP_VERSION="5.4.14"
     
     echo -e "-----> Unbundling binary package"
     
     mkdir -p /app/vendor
-    mkdir -p /app/vendor/php
+    base=/app/vendor
 
     7z=/app/vendor/p7zip/bin/7z
     ex="e"
     # package.tar.gz contains lua, lua-jit and nginx and friends
-    $7z e $ROCK_DIR/packages/dist/php.7z
-    $7z e $ROCK_DIR/packages/dist/lua.7z
-    $7z e $ROCK_DIR/packages/dist/luajit.7z
-    $7z e $ROCK_DIR/packages/dist/lualib.7z
-    $7z e $ROCK_DIR/packages/dist/nginx.7z
+    $7z e $ROCK_DIR/packages/dist/php.7z -o$base/php
+    $7z e $ROCK_DIR/packages/dist/lua.7z -o$base/lua
+    $7z e $ROCK_DIR/packages/dist/luajit.7z -o$base/luajit
+    $7z e $ROCK_DIR/packages/dist/lualib.7z -o$base/lualib
+    $7z e $ROCK_DIR/packages/dist/nginx.7z -o$base/nginx
 
     echo -e "Finished installing the binary bundle" | indent
 }
