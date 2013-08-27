eb62b2d3ba22cb4220299d62bb7fc3852c01ed5c
With Jive <withjive@icloud.com>
2013-08-27 08:10:13 +0700

    adding precompiled lzma 7z

--- packages/install.p7zip.sh
+++ packages/install.p7zip.sh
@@ -3,9 +3,16 @@
 
 function install_p7zip(){
 
-    cd $ROCK_DIR/3rdparty/p7zip
-    echo -e "Compiling p7zip" | indent
-    make -j4 -s
-    echo -e "Installing p7zip" | indent
-    make install -s
+    # cd $ROCK_DIR/3rdparty/p7zip
+    # echo -e "Compiling p7zip" | indent
+    # make -j4 -s
+    # echo -e "Installing p7zip" | indent
+    # make install -s
+    proot=/app/vendor/p7zip/bin
+
+    mkdir -p $proot
+    cd $proot
+
+    cp $ROCK_DIR/packages/dist/7za.tar.gz ./
+    tar -xf 7za.tar.gz
 }
